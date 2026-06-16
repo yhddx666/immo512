73f81f9747d5ed009d0dd5b11be94008b7201b2f
yhddx666 <84658538+yhddx666@users.noreply.github.com>
2026-06-16 14:26:31 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -17,7 +17,7 @@
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 #echo 'src-git messense https://github.com/messense/aliyundrive-webdav' >>feeds.conf.default
-git clone https://github.com/messense/aliyundrive-webdav package/messense
+#git clone https://github.com/messense/aliyundrive-webdav package/messense
 
 ## 修改DTS的ubi为490MB的0x1ea00000
 sed -i 's/reg = <0x600000 0x6e00000>/reg = <0x600000 0x1ea00000>/' target/linux/mediatek/dts/mediatek/mt7986a-xiaomi-redmi-router-ax6000-ubootmod.dts
