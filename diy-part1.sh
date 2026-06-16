8464523574639cbc268037b7cc6c3dea963c0608
yhddx666 <84658538+yhddx666@users.noreply.github.com>
2026-06-16 14:26:15 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -20,6 +20,6 @@
 git clone https://github.com/messense/aliyundrive-webdav package/messense
 
 ## 修改DTS的ubi为490MB的0x1ea00000
-sed -i 's/reg = <0x600000 0x6e00000>/reg = <0x600000 0x1ea00000>/' target/linux/mediatek/files-5.4/arch/arm64/boot/dts/mediatek/mt7986a-xiaomi-redmi-router-ax6000-uboot.dts
+sed -i 's/reg = <0x600000 0x6e00000>/reg = <0x600000 0x1ea00000>/' target/linux/mediatek/dts/mediatek/mt7986a-xiaomi-redmi-router-ax6000-ubootmod.dts
 ## 修改DTS的spi_nand的spi-max-frequency为52MHz，52000000
-sed -i 's/spi-max-frequency = <20000000>/spi-max-frequency = <52000000>/' target/linux/mediatek/files-5.4/arch/arm64/boot/dts/mediatek/mt7986a-xiaomi-redmi-router-ax6000-uboot.dts
+sed -i 's/spi-max-frequency = <20000000>/spi-max-frequency = <52000000>/' target/linux/mediatek/dts/mediatek/mt7986a-xiaomi-redmi-router-ax6000-ubootmod.dts
